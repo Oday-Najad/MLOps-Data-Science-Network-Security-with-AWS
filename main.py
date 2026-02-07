5cf0a1b02a0ea8d0aaebb9d0c1706754e895067b
Oday-Najad <oday.najad@lau.edu>
2026-02-07 21:18:18 +0100

    mlflow tracking

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.config_entity import DataTransformationConfig
 
+
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 import sys
 
 
@@ -36,6 +40,14 @@ if __name__ == "__main__":
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        logging.info("Model Training stared")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,
+                                     data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         logging.info(f"Error occurred in main.py: {e}")
